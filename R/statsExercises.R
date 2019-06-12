1265f89c637557543ef4f2ed35449d4eb716713f
Ron Wehrens <ron.wehrens@gmail.com>
2019-06-12 09:23:12 +0200

    used dot for faster pairs plotting

--- R/statsExercises.R
+++ R/statsExercises.R
@@ -141,7 +141,7 @@ ExPlot <- function() {
 
     if (ans == 1)
         print(splom(~ wines.df0[,-1], data = wines.df0,
-                    groups = variety, pscales = 0))
+                    pch = ".", cex = 2, groups = variety, pscales = 0))
     
     if (ans == 2)
         print(bwplot(variety ~ Level | Var, data = wines.df,
@@ -227,10 +227,10 @@ ExPlot <- function() {
       
       if (ans == 1) {
         print(splom(wines.df0[,-1], groups = vintages, pscales = 0,
-                    main = "Original data"))
+                    pch = ".", cex = 2, main = "Original data"))
         dev.new()
         print(splom(wines.df0.sc[,-1], groups = vintages, pscales = 0,
-                    main = "Scaled data"))
+                    pch = ".", cex = 2, main = "Scaled data"))
       }
       
       if (ans == 2) {
@@ -292,7 +292,7 @@ ExPCA <- function(){
   
   readline("\n\nHit return to show a scatter plot of the data (be patient!)")
   
-  pairs(PCADATA, col = colors)
+  pairs(PCADATA, pch = ".", cex = 2, col = colors)
   
   cat("\nCan you guess what you are looking at?")
   
